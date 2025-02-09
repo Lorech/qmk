83ee3d7ae00454e72645e51c65057ef2a63db87b
Lorech <laurisdedumets@gmail.com>
2025-02-09 19:16:12 +0200

    crkbd: Fix build

--- users/lorech/lorech.c
+++ users/lorech/lorech.c
@@ -5,20 +5,20 @@
  * See <https://www.gnu.org/licenses/> for details.
  */
 
-#ifdef TRI_LAYER_ENABLE
-    #include "layers.h"
-    #include "tri_layer.h"
-#endif // !TRI_LAYER_ENABLE
 #ifdef OS_DETECTION_ENABLE
     #include "keycode_config.h"
     #include "os_detection.h"
 #endif // !OS_DETECTION_ENABLE
-
-void keyboard_pre_init_user() {
 #ifdef TRI_LAYER_ENABLE
-    set_tri_layer_layers(_LOWER, _UPPER, _ADJUST);
+    #include "layers.h"
+    #include "tri_layer.h"
 #endif // !TRI_LAYER_ENABLE
+
+#ifdef TRI_LAYER_ENABLE
+void keyboard_pre_init_user() {
+    set_tri_layer_layers(_LOWER, _UPPER, _ADJUST);
 }
+#endif // !TRI_LAYER_ENABLE
 
 #ifdef OS_DETECTION_ENABLE
 bool process_detected_host_os_user(os_variant_t detected_os) {
--- users/lorech/rules.mk
+++ users/lorech/rules.mk
@@ -1,4 +1,7 @@
 SRC += $(USER_PATH)/lorech.c \
        $(USER_PATH)/keyrecords/mods.c \
-       $(USER_PATH)/keyrecords/process_records.c \
-       $(USER_PATH)/keyrecords/tap_dance.c
+       $(USER_PATH)/keyrecords/process_records.c
+
+ifeq ($(strip $(TAP_DANCE_ENABLE)), yes)
+SRC += $(USER_PATH)/keyrecords/tap_dance.c
+endif
