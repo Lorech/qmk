8d2aec9961d0991ae33fd684f41cf03623950e61
Lorech <laurisdedumets@gmail.com>
2025-09-26 09:52:08 +0300

    homerow: Define homerow mods

--- users/lorech/keyrecords/process_records.h
+++ users/lorech/keyrecords/process_records.h
@@ -33,3 +33,23 @@ void post_process_record_keymap(uint16_t keycode, keyrecord_t *record);
 #define QWERTY  PDF(_QWERTY)
 #define CLMAKDH PDF(_COLEMAK_DH)
 #define PLOVER  PDF(_PLOVER)
+
+// QWERTY home row mods
+#define QHR_A   LGUI_T(KC_A)
+#define QHR_S   LALT_T(KC_S)
+#define QHR_D   LCTL_T(KC_D)
+#define QHR_F   LSFT_T(KC_F)
+#define QHR_J   RSFT_T(KC_J)
+#define QHR_K   RCTL_T(KC_K)
+#define QHR_L   LALT_T(KC_L)
+#define QHR_SCL RGUI_T(KC_SCLN)
+
+// COLEMAK-DH home row mods
+#define CHR_A LGUI_T(KC_A)
+#define CHR_R LALT_T(KC_R)
+#define CHR_S LCTL_T(KC_S)
+#define CHR_T LSFT_T(KC_T)
+#define CHR_N RSFT_T(KC_N)
+#define CHR_E RCTL_T(KC_E)
+#define CHR_I LALT_T(KC_I)
+#define CHR_O RGUI_T(KC_O)
