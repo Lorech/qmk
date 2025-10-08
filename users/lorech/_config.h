6714f3b1670df2ccb8e9f045f0a1767472097151
Lorech <laurisdedumets@gmail.com>
2025-10-08 10:09:19 +0300

    global: Define shared space for config.h modifications
    After some testing of feature implementations in upcoming commits, I
    found that there are some configurations that work _only_ when defined
    within config.h. From Drashna's repository, I found an undocumented way
    to globally append things to config.h at a userspace-level, which can
    then globally adjust these values for all keyboards.
    
    This header should be used exclusively for these special configurations
    - more specific modifications shall remain separated within their
    separate headers and source files.

---
+++ users/lorech/_config.h
@@ -0,0 +1,9 @@
+/**
+ * Copyright 2025 Lauris Patriks Dedumets
+ *
+ * Licensed under the GNU General Public License, version 2 or later.
+ * See <https://www.gnu.org/licenses/> for details.
+ */
+
+#pragma once
+
--- users/lorech/rules.mk
+++ users/lorech/rules.mk
@@ -1,3 +1,5 @@
+CONFIG_H += $(USER_PATH)/_config.h
+
 SRC += $(USER_PATH)/lorech.c \
        $(USER_PATH)/keyrecords/mods.c \
        $(USER_PATH)/keyrecords/process_records.c
