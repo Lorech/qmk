88db148577f75d5022fa69086beb9f3878166cd1
Lorech <laurisdedumets@gmail.com>
2025-10-08 10:23:13 +0300

    capsword: Prevent shifted dash

--- users/lorech/keyrecords/caps_word.c
+++ users/lorech/keyrecords/caps_word.c
@@ -18,13 +18,13 @@ bool caps_word_press_user(uint16_t keycode) {
     switch (keycode) {
         // Continue Caps Word, applying Shift.
         case KC_A ... KC_Z:
-        case KC_MINS:
             add_weak_mods(MOD_BIT(KC_LSFT));
             return true;
         // Continue Caps Word.
         case KC_1 ... KC_0:
         case KC_BSPC:
         case KC_DEL:
+        case KC_MINS:
         case KC_UNDS:
             return true;
         // Deactivate Caps Word.
